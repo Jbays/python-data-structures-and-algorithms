ce6a250441ae3318bda44789b47d2a79028f21b1
jbays <jjbaize@gmail.com>
2017-04-16 19:05:48 -0500

    Implemented far superior solution

--- binarySearch.py
+++ binarySearch.py
@@ -4,10 +4,10 @@
 myList = [2, 5, 7, 12, 14, 21, 28, 31, 36]
 
 # name: binarySearch
-# description:
-# param0 searchList: The ORDERED list of values you wish to search
-# param1 searchItem: The value you wish to find
-# returns: 
+# description: Searches for param0 inside param1
+# param0 item: The value you wish to find
+# param1 list: The ORDERED list of values you wish to search
+# returns: If found, true.  Else returns false
 
 
 def binarySearch(item,list):
@@ -28,43 +28,4 @@ def binarySearch(item,list):
 	print found
 	return found
 
-# 	counter+=1
-
-# 	print "You are searching for this value>>>",searchItem
-# 	listMidpoint = len(aList)/2
-# 	midPointVal = aList[listMidpoint]
-
-# 	splitLists = split_list(aList)
-# 	print splitLists
-# 	print "this is the midPointValue",aList[listMidpoint]
-
-# 	#case 1: match
-# 	if searchItem == midPointVal:
-# 		print "match!"
-# 		print "number of searches",counter
-
-# 	else:
-# 		print "no match -- will continue search!"
-		
-# 		#case 2: searchItem is greater
-# 		if searchItem > midPointVal:
-# 			print "searchItem is greater than midPointVal"
-# 			binarySearch(splitLists[1],searchItem,counter)
-
-# 		#case 3: searchItem is lesser
-# 		elif searchItem < midPointVal:
-# 			print "searchItem is less than midPointVal"
-# 			binarySearch(splitLists[0],searchItem,counter)
-
-# 		#case 4: not found
-# 		else:
-# 			print "your value is not present"
-# 			print "Searched list this many times>>>>",counter
-
-
-# def split_list(a_list):
-#     half = len(a_list)/2
-#     return a_list[:half], a_list[half:]
-		
-# Searches for the second parameter inside the first
 binarySearch(12,myList)
