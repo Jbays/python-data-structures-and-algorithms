ed5475819e8231dab5f2d2fa40751aa37263674a
jbays <jjbaize@gmail.com>
2017-04-16 19:03:01 -0500

    Better solution

--- binarySearch.py
+++ binarySearch.py
@@ -10,44 +10,61 @@ myList = [2, 5, 7, 12, 14, 21, 28, 31, 36]
 # returns: 
 
 
-def binarySearch(aList,searchItem,counter):
-	counter+=1
+def binarySearch(item,list):
+	found = False
+	bottom = 0
+	top = len(list)-1
 
-	print "You are searching for this value>>>",searchItem
-	listMidpoint = len(aList)/2
-	midPointVal = aList[listMidpoint]
-
-	splitLists = split_list(aList)
-	print splitLists
-	print "this is the midPointValue",aList[listMidpoint]
-
-	#case 1: match
-	if searchItem == midPointVal:
-		print "match!"
-		print "number of searches",counter
-
-	else:
-		print "no match -- will continue search!"
-		
-		#case 2: searchItem is greater
-		if searchItem > midPointVal:
-			print "searchItem is greater than midPointVal"
-			binarySearch(splitLists[1],searchItem,counter)
-
-		#case 3: searchItem is lesser
-		elif searchItem < midPointVal:
-			print "searchItem is less than midPointVal"
-			binarySearch(splitLists[0],searchItem,counter)
-
-		#case 4: not found
+	while bottom <= top and not found:
+		middle = (bottom+top)//2
+		if myList[middle]==item:
+			found = True
+		elif myList[middle] < item:
+			bottom = middle+1
 		else:
-			print "your value is not present"
-			print "Searched list this many times>>>>",counter
+
+			top = middle-1
+
+	print found
+	return found
+
+# 	counter+=1
+
+# 	print "You are searching for this value>>>",searchItem
+# 	listMidpoint = len(aList)/2
+# 	midPointVal = aList[listMidpoint]
+
+# 	splitLists = split_list(aList)
+# 	print splitLists
+# 	print "this is the midPointValue",aList[listMidpoint]
+
+# 	#case 1: match
+# 	if searchItem == midPointVal:
+# 		print "match!"
+# 		print "number of searches",counter
+
+# 	else:
+# 		print "no match -- will continue search!"
+		
+# 		#case 2: searchItem is greater
+# 		if searchItem > midPointVal:
+# 			print "searchItem is greater than midPointVal"
+# 			binarySearch(splitLists[1],searchItem,counter)
+
+# 		#case 3: searchItem is lesser
+# 		elif searchItem < midPointVal:
+# 			print "searchItem is less than midPointVal"
+# 			binarySearch(splitLists[0],searchItem,counter)
+
+# 		#case 4: not found
+# 		else:
+# 			print "your value is not present"
+# 			print "Searched list this many times>>>>",counter
 
 
-def split_list(a_list):
-    half = len(a_list)/2
-    return a_list[:half], a_list[half:]
+# def split_list(a_list):
+#     half = len(a_list)/2
+#     return a_list[:half], a_list[half:]
 		
 # Searches for the second parameter inside the first
-binarySearch(myList,99,0)
+binarySearch(12,myList)
