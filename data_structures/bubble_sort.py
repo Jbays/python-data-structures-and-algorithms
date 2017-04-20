eea1c45b51f3519c99e23646d0b3e2bffe2013f8
jbays <jjbaize@gmail.com>
2017-04-19 23:13:32 -0500

    Super close to functional implementation

--- data_structures/bubble_sort.py
+++ data_structures/bubble_sort.py
@@ -12,14 +12,34 @@ import random
 # 1
 # 12, 5, 7, 18, 11, 6, 12, 4, 17, 1
 
-def bubble_sort(list):
-	print list
-	counter = 0
+def bubble_sort(list,counter):
+	print "here's the input list>>>>>>>>>>", list
+	
 	for num in list:
-		counter=counter+1
 
+		left = num
+		try:
+			right = list[list.index(num)+1]
+			
+			if (left > right):
+				# print "Left is greater than right!  Swap your variables!"
+				counter = counter+1
+				left_position = list.index(left)
+				right_position = list.index(right)
+				temp = left_position
+
+				print list[left_position]
+				print list[right_position]
+
+				# list[left_position] = list[right_position]
+				# list[right_position] = temp
+
+				return bubble_sort(list,counter)
+		except:
+			print "# of required bubble-sorts",counter
+			print "this list should be sorted>>>>>",list
 
 sample_list = range(1,17)
 random.shuffle(sample_list)
 
-bubble_sort(sample_list)
+bubble_sort(sample_list,0)
