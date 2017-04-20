c0102928a00b29a4d4c9470e44b2e4a28a510f7a
jbays <jjbaize@gmail.com>
2017-04-19 21:34:24 -0500

    Finished first set of tests

--- dataStructures/trialTests.py
+++ dataStructures/trialTests.py
@@ -20,8 +20,11 @@ class TestStringMethods(unittest.TestCase):
 	#check that the binary functions as intended
 	def test_binary_search_tree(self):
 		list = range(1,17)
-		print(binarySearch.binarySearch(1,list))
-		print(binarySearch.binarySearch(8,list))
+		self.assertEqual(binarySearch.binarySearch(8,list),(True,1))
+		self.assertEqual(binarySearch.binarySearch(1,list),(True,4))
+		self.assertEqual(binarySearch.binarySearch(39,list),(False,5))
+		self.assertEqual(binarySearch.binarySearch(-34,list),(False,4))
+
 
 if __name__ == '__main__':
 	unittest.main()
