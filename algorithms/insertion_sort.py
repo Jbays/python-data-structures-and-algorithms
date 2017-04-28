88e83fc1d69823f898abcb679f2e4659cd66d165
jbays <jjbaize@gmail.com>
2017-04-28 10:49:57 -0500

    The break was incorrectly scoped.  MVP complete '

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -10,32 +10,16 @@ import random
 # returns: sorted list
 
 def insertion_sort(sorted_list,list):
-
 	if len(list) > 0:
-		print "here is the sorted_list",sorted_list
-		print "here is the list",list
-		print "+++++++++++++++++++++++"
-
 		# the first number is automatically put into the sorted_list
 		if len(sorted_list) == 0:
 			sorted_list.append(list.pop())
 			insertion_sort(sorted_list,list)
 		elif len(sorted_list) > 0:
-			# print "sorted_list:",sorted_list
-			# print "list:",list
-			print "before list",list
 			to_be_sorted = list.pop()
-			print "after  list",list
-			print "to_be_sorted:",to_be_sorted
-
-			print "before sorted_list",sorted_list
 			sorted_list = sort_unsorted_num(to_be_sorted,sorted_list)
-			print "after  sorted_list",sorted_list
 
-
-
-		
-
+			insertion_sort(sorted_list,list)
 	else:
 		print "this list should be sorted",sorted_list
 		print "this list should be empty",list
@@ -44,29 +28,15 @@ def sort_unsorted_num(num,list):
 	num_is_sorted = False
 	for sorted_num in list:
 		sorted_index = list.index(sorted_num)
-		print sorted_num
-		print sorted_index
 		if num < sorted_num:
 			list.insert(sorted_index,num)
 			num_is_sorted = True
-		break
+			break
 
 	if num_is_sorted == False:
 		list.append(num)
 
-	# print list
 	return list
-
-	# 4-23-2017
-	# something about this conditional causes an infinite loop.
-	# not sure what's wrong.  The logic is close to working.
-		
-	# 4-28-2017
-	# I can visualize this solution implemented with recursion.
-	# Once one instance of sorting has occured,
-	# Remove that number from the original list
-	# And pass both sorted & unsorted list to the original function
-		
 
 sample_list = range(1,17)
 random.shuffle(sample_list)
